d3dd810c45f5f1f5249480ca1522c5f55eb23e98
Deepak Jha3 <deepak.jha3@ibm.com>
2023-06-19 09:46:22 +0530

    Added binary search using recursion

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import { letterCombinations } from "./Recursion/LetterCombinations";
-console.log(letterCombinations("23"));
+import { RatInMaze } from "./Recursion/ratInMaze";
+
+const rat = new RatInMaze();
+
+console.log(
+  rat.findPath(
+    [
+      [1, 0, 0, 0],
+      [1, 1, 0, 1],
+      [1, 1, 0, 0],
+      [0, 1, 1, 1],
+    ],
+    4
+  )
+);
